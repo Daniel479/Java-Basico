b8ed046e635a123292a6833d5ad8b5990880bc51
Daniel <dvieiraornelas45@gmail.com>
2024-10-08 11:38:20 -0300

    contando numero de funcionarios

---
+++ Estruturas de repetição/src/Numfunc.java
@@ -0,0 +1,7 @@
+public class Numfunc{
+    public static void main(String[] args) {
+        for ( int funcionarios = 1; funcionarios <=35; funcionarios ++){
+            System.out.println("Contando funcionarios" + funcionarios);
+        }
+    }
+}
